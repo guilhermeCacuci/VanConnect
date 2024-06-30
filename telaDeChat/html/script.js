9d27803a831cf7dbe6bbaaaefed588ca2e9ff962
Guilherme Caçuci <126695695+guilhermeCacuci@users.noreply.github.com>
2024-06-30 17:58:08 -0300

    Add files via upload

---
+++ telaDeChat/html/script.js
@@ -0,0 +1,62 @@
+var socket;
+var usernameInput;
+var chatIDInput;
+var messageInput;
+var chatRoom;
+var dingSound;
+var messages = [];
+var delay = true;
+
+function onload(){
+  socket = io();
+  usernameInput = document.getElementById("NameInput");
+  chatIDInput = document.getElementById("IDInput");
+  messageInput = document.getElementById("ComposedMessage");
+  chatRoom = document.getElementById("RoomID");
+  dingSound = document.getElementById("Ding");
+
+  socket.on("join", function(room){
+    chatRoom.innerHTML = "Chatroom : " + room;
+  });
+
+  socket.on("recieve", function(message){
+    console.log(message);
+    if (messages.length < 9){
+      messages.push(message);
+      dingSound.currentTime = 0;
+      dingSound.play();
+    } else {
+      messages.shift();
+      messages.push(message);
+    }
+    updateMessages();
+  });
+}
+
+function Connect(){
+  socket.emit("join", chatIDInput.value, usernameInput.value);
+}
+
+function Send(){
+  if (delay && messageInput.value.replace(/\s/g, "") != ""){
+    delay = false;
+    setTimeout(delayReset, 1000);
+    socket.emit("send", messageInput.value);
+    messageInput.value = "";
+  }
+}
+
+function delayReset(){
+  delay = true;
+}
+
+function updateMessages(){
+  const chat = document.getElementById("Chat");
+  chat.innerHTML = ""; // Clear current messages
+  messages.forEach((msg, index) => {
+    const messageElement = document.createElement("p");
+    messageElement.classList.add("message");
+    messageElement.textContent = msg;
+    chat.appendChild(messageElement);
+  });
+}
